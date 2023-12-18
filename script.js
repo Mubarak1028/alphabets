b12f2542c03f603ebd4d236bbd83608c526f0d56
mubarak usman <mubarak725908@gmail.com>
2023-12-18 19:05:20 +0100

    adding js and css files

---
+++ script.js
@@ -0,0 +1,79 @@
+var image = document.querySelector('#alphabet-image')
+document.querySelector('#a').addEventListener('click', function(){
+    image.src = 'images/alpha_a.jpeg'
+})
+document.querySelector('#b').addEventListener('click', function(){
+    image.src = 'images/alpha_b.jpeg'
+})
+document.querySelector('#c').addEventListener('click', function(){
+    image.src = 'images/alpha_c.jpeg'
+})
+document.querySelector('#d').addEventListener('click', function(){
+    image.src = 'images/alpha_d.jpeg'
+})
+document.querySelector('#e').addEventListener('click', function(){
+    image.src = 'images/alpha_e.jpeg'
+})
+document.querySelector('#f').addEventListener('click', function(){
+    image.src = 'images/alpha_f.jpeg'
+})
+document.querySelector('#g').addEventListener('click', function(){
+    image.src = 'images/alpha_g.jpeg'
+})
+document.querySelector('#h').addEventListener('click', function(){
+    image.src = 'images/alpha_h.jpeg'
+})
+document.querySelector('#i').addEventListener('click', function(){
+    image.src = 'images/alpha_i.jpeg'
+})
+document.querySelector('#j').addEventListener('click', function(){
+    image.src = 'images/alpha_j.jpeg'
+})
+document.querySelector('#k').addEventListener('click', function(){
+    image.src = 'images/alpha_k.jpeg'
+})
+document.querySelector('#l').addEventListener('click', function(){
+    image.src = 'images/alpha_l.jpeg'
+})
+document.querySelector('#m').addEventListener('click', function(){
+    image.src = 'images/alpha_m.jpeg'
+})
+document.querySelector('#n').addEventListener('click', function(){
+    image.src = 'images/alpha_n.jpeg'
+})
+document.querySelector('#o').addEventListener('click', function(){
+    image.src = 'images/alpha_o.jpeg'
+})
+document.querySelector('#p').addEventListener('click', function(){
+    image.src = 'images/alpha_p.jpeg'
+})
+document.querySelector('#q').addEventListener('click', function(){
+    image.src = 'images/alpha_q.jpeg'
+})
+document.querySelector('#r').addEventListener('click', function(){
+    image.src = 'images/alpha_r.jpeg'
+})
+document.querySelector('#s').addEventListener('click', function(){
+    image.src = 'images/alpha_s.jpeg'
+})
+document.querySelector('#t').addEventListener('click', function(){
+    image.src = 'images/alpha_t.jpeg'
+})
+document.querySelector('#u').addEventListener('click', function(){
+    image.src = 'images/alpha_u.jpeg'
+})
+document.querySelector('#v').addEventListener('click', function(){
+    image.src = 'images/alpha_v.jpeg'
+})
+document.querySelector('#w').addEventListener('click', function(){
+    image.src = 'images/alpha_w.jpeg'
+})
+document.querySelector('#x').addEventListener('click', function(){
+    image.src = 'images/alpha_x.jpeg'
+})
+document.querySelector('#y').addEventListener('click', function(){
+    image.src = 'images/alpha_y.jpeg'
+})
+document.querySelector('#z').addEventListener('click', function(){
+    image.src = 'images/alpha_z.jpeg'
+})
